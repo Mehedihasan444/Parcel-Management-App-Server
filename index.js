c8cda06405943c2f6e20604d7f0f480cf43e8371
Mehedihasan444 <mehedihasan67705251@gmail.com>
2023-12-27 02:57:18 +0600

    adding few more api

--- index.js
+++ index.js
@@ -32,7 +32,51 @@ async function run() {
 
     const userCollection = client.db("parcelManagementDB").collection("users");
     const bookingCollection = client.db("parcelManagementDB").collection("bookings");
+    const reviewCollection = client.db("parcelManagementDB").collection("reviews");
     const paymentCollection = client.db("parcelManagementDB").collection("payments");
+
+
+
+
+    // jwt related api
+    app.post('/api/v1/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+
+
+
+
 
     // user related api
     app.post("/api/v1/users", async (req, res) => {
@@ -62,24 +106,31 @@ async function run() {
       const data = req.body;
       const filter = { email: email };
       if (!data.role) {
-      const updatedDoc = {
-        $set: {
-          name: data.name,
-          email: data.email,
-          phone: data.phone,
-          image: data.image,
-        },
-      };
+        const updatedDoc = {
+          $set: {
+            name: data.name,
+            email: data.email,
+            phone: data.phone,
+            image: data.image,
+          },
+        };
       }
       const updatedDoc = {
         $set: {
           role: data.role,
         },
       };
-      
+
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
+    app.post("/api/v1/users/reviews", async (req, res) => {
+      const user = req.body;
+      const result = await reviewCollection.insertOne(user);
+      res.send(result);
+    });
+
+
 
     // user booking related api
     app.post("/api/v1/users/bookings", async (req, res) => {
@@ -93,12 +144,19 @@ async function run() {
       const result = await bookingCollection.find().toArray();
       console.log(result);
       res.send(result);
-    });
+    }); ///---------------
     app.get("/api/v1/users/bookings/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await bookingCollection.find(query).toArray();
       // console.log(result)
+      res.send(result);
+    });
+    app.get("/api/v1/users/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.findOne(query);
+      console.log(result);
       res.send(result);
     });
     app.delete("/api/v1/users/bookings/:id", async (req, res) => {
@@ -110,46 +168,91 @@ async function run() {
     });
     app.patch("/api/v1/users/bookings/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-         status:'On The Way'
+          status: "On The Way",
+          deliveryMenID: id,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.patch("/api/v1/users/updateBooking/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          phone: data?.phone,
+          parcelType: data?.parcelType,
+          receiverName: data?.receiverName,
+          weight: data?.weight,
+          receiverPhone: data?.receiverPhone,
+          deliveryAddressLatitude: data?.deliveryAddressLatitude,
+          deliveryAddressLongitude: data?.deliveryAddressLongitude,
+          requestedDeliveryDate: data?.requestedDeliveryDate,
+          bookingDate: data?.bookingDate,
+          price: data?.price,
         },
       };
       const result = await bookingCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
 
-
-
     // admin related api
-    app.get("/api/v1/admin/deliveryMens", async (req, res) => {
-      const query={role: 'deliveryMen'}
-      const result = await userCollection.find(query).toArray();
+    app.get("/api/v1/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { 
+        email: email,
+        role: "admin"
+       };
+      const result = await userCollection.findOne(query);
       console.log(result);
-      res.send(result);
+      if (result) {
+        res.send({admin:true});
+        
+      }
+      else {
+        res.send({admin:false});
+      }
+      // res.send(result);
     });
-
-
+   
 
     // DeliveryMen related api
-    app.get("/api/v1/deliveryMen/deliveryList/:email", async (req, res) => {
-     
-    
+    app.get("/api/v1/deliveryMen/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { 
+        email: email,
+        role: "deliveryMen"
+       };
+      const result = await userCollection.findOne(query);
+      //console.log(result);
+      if (result) {
+        res.send({deliveryMen:true});
+        
+      }
+      else {
+        res.send({deliveryMen:false});
+      }
+      // res.send(result);
     });
-    app.get("/api/v1/deliveryMen/delivery/reviews/:email", async (req, res) => {
-     
-    
+    app.get("/api/v1/users/admin/deliveryMens", async (req, res) => {
+      const query = { role: "deliveryMen" };
+      const result = await userCollection.find(query).toArray();
+      //console.log(result);
+      res.send(result);
     });
-    app.delete("/api/v1/deliveryMen/deliveryList/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-     
-    
-    });
-
-
-
+    // app.get("/api/v1/deliveryMen/deliveryList/:email", async (req, res) => {});
+    // app.get(
+    //   "/api/v1/deliveryMen/delivery/reviews/:email",
+    //   async (req, res) => {}
+    // );
+    // app.delete("/api/v1/deliveryMen/deliveryList/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
