152e7123b9fc6ba27f2d8cd6d1255eb108bce8c1
Mehedihasan444 <mehedihasan67705251@gmail.com>
2023-12-26 13:38:53 +0600

    adding update role api and update booking status api

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const jwt = require('jsonwebtoken');
-const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const app = express();
 const port = process.env.PORT || 5000;
 
-
-
-
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-  ],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DATABASE_LOCAL_USERNAME}:${process.env.DATABASE_LOCAL_PASSWORD}@cluster0.6egtgqe.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,7 +22,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -40,47 +34,138 @@ async function run() {
     const bookingCollection = client.db("parcelManagementDB").collection("bookings");
     const paymentCollection = client.db("parcelManagementDB").collection("payments");
 
+    // user related api
+    app.post("/api/v1/users", async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists:
+      // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/api/v1/users/admin", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/api/v1/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+    app.patch("/api/v1/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const data = req.body;
+      const filter = { email: email };
+      if (!data.role) {
+      const updatedDoc = {
+        $set: {
+          name: data.name,
+          email: data.email,
+          phone: data.phone,
+          image: data.image,
+        },
+      };
+      }
+      const updatedDoc = {
+        $set: {
+          role: data.role,
+        },
+      };
+      
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-
-    app.post('/api/v1/users/bookings',async (req,res)=>{
-
+    // user booking related api
+    app.post("/api/v1/users/bookings", async (req, res) => {
       const data = req.body;
       // console.log(data);
       const result = await bookingCollection.insertOne(data);
       res.send(result);
-    })
+    });
 
-    app.get('/api/v1/users/bookings/:email',async (req,res)=>{
-const email = req.params.email;
-const query = {email:email}
-            const result = await bookingCollection.find(query).toArray();
-            console.log(result)
+    app.get("/api/v1/users/admin/bookings", async (req, res) => {
+      const result = await bookingCollection.find().toArray();
+      console.log(result);
       res.send(result);
-    })
-    app.delete('/api/v1/users/bookings/:id', async(req,res)=>{
+    });
+    app.get("/api/v1/users/bookings/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await bookingCollection.find(query).toArray();
+      // console.log(result)
+      res.send(result);
+    });
+    app.delete("/api/v1/users/bookings/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await bookingCollection.deleteOne(query);
-      console.log(result)
+      // console.log(result)
       res.send(result);
-    })
-   
+    });
+    app.patch("/api/v1/users/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+         status:'On The Way'
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+
+
+    // admin related api
+    app.get("/api/v1/admin/deliveryMens", async (req, res) => {
+      const query={role: 'deliveryMen'}
+      const result = await userCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+
+
+    // DeliveryMen related api
+    app.get("/api/v1/deliveryMen/deliveryList/:email", async (req, res) => {
+     
+    
+    });
+    app.get("/api/v1/deliveryMen/delivery/reviews/:email", async (req, res) => {
+     
+    
+    });
+    app.delete("/api/v1/deliveryMen/deliveryList/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+     
+    
+    });
+
 
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-app.get('/', (req, res) => {
-  res.send('Parcel management server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Parcel management server is running");
+});
 
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
-})
+});
